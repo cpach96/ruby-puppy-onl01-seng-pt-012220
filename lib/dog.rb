0d1064ac2fe7f5fb505ca1b4a574b2abc5c6c315
Curtis pachtinger <cpach96@gmail.com>
2020-02-11 00:52:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,15 +5,16 @@ class Dog
   
   attr_accessor :name
   
-  
-  
   def initialize(name)
     @name = name
-    @@all << self
 end
 
   def self.all
     @@all
+  end
+  
+  def self.save
+    @@all << self
   end
   
 
