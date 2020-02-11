525b7830f1eb1241f72805c1c419c80f92cb37cc
Curtis pachtinger <cpach96@gmail.com>
2020-02-11 00:08:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,20 @@
 # Add your code here
 class Dog 
   
+  @@all = []
+  
   def initialize(name)
     @name = name
-    @@all = 
+    @@all << name
+end
+
+  def self.all
+    @@all
+  end
   
+  def self.print_all
+    
+    
+  end
+
 end
